9bf943c2031e46349464c337291fb64c137358ec
maria alvarez <mar.alvareg@gmail.com>
2020-11-27 22:14:16 -0300

    initial

---
+++ app/models/post.rb
@@ -0,0 +1,13 @@
+class Post < ApplicationRecord
+  belongs_to :author
+  has_many :elements
+
+  has_one_attached :header_image
+
+  validates :title, uniqueness: true
+  validates_presence_of :title
+
+  scope :published, -> do
+    where(published: true)
+  end
+end
