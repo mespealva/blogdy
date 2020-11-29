6d8cd4c83efa1b2aff7a03a960c8a6fc43a1cc98
maria alvarez <mar.alvareg@gmail.com>
2020-11-28 21:59:35 -0300

    slug

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,10 @@
 class Post < ApplicationRecord
+  extend FriendlyId
+  friendly_id :title, use: [:slugged, :history, :finders]
+  
   belongs_to :author
   has_many :elements, -> { order(position: :asc) }
-
+  
   has_one_attached :header_image
 
   validates :title, uniqueness: true
@@ -10,4 +13,9 @@ class Post < ApplicationRecord
   scope :published, -> do
     where(published: true)
   end
+
+  def should_generate_new_friendly_id?
+    title_changed?
+  end
+
 end
