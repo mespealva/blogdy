4790e3cfef7a44d4aa194a3da03771cac1e8ca55
maria alvarez <mar.alvareg@gmail.com>
2020-12-01 15:03:17 -0300

    terminado navbar

--- app/controllers/authors/posts_controller.rb
+++ app/controllers/authors/posts_controller.rb
@@ -1,6 +1,6 @@
 module Authors
   class PostsController < AuthorsController
-    before_action :set_post, only: [:edit, :update, :destroy, :publish, :unpublish]
+    before_action :set_post, only: [:edit, :update, :destroy, :publish, :unpublish, :finish]
     
   
     # GET /posts
